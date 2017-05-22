65e9ff8099afaf25942f730a971b195e0fbfe63b
Thomas Wilburn <news@thomaswilburn.net>
2017-05-22 11:55:32 -0700

    Loading color buffer now

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ var gl = canvas.getContext("webgl");
 
 gl.enable(gl.DEPTH_TEST);
 // gl.enable(gl.CULL_FACE);
-gl.clearColor(0, 0, 1, 1);
+gl.clearColor(0, .5, 1, 1);
 gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
 var vertex = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertex, `
 attribute vec3 a_position;
+attribute vec3 a_color;
+
 uniform mat4 u_perspective;
 uniform mat4 u_matrix;
+
 varying vec4 v_screenspace;
 varying vec3 v_position;
+varying vec3 v_color;
 
 void main() {
   v_position = a_position;
+  v_color = a_color;
   v_screenspace = u_perspective * u_matrix * vec4(a_position, 1.0);
   gl_Position = v_screenspace;
 }
@@ -30,9 +35,12 @@ precision mediump float;
 
 varying vec4 v_screenspace;
 varying vec3 v_position;
+varying vec3 v_color;
 
 void main() {
-  gl_FragColor = vec4(v_position.yyy / 2.0, 1.0);
+  vec3 shade = v_position.yyy / 2.0;
+  vec3 color = vec3(1.0, 1.0, v_color.r) * shade;
+  gl_FragColor = vec4(color, 1.0);
 }
 `);
 gl.compileShader(fragment);
@@ -44,32 +52,28 @@ gl.linkProgram(program);
 
 gl.useProgram(program);
 
-var verts = [
-  0.0, 1.0, 0.0,
-  -1.0, -1.0, 0.0,
-  1.0, -1.0, 0.0
-];
-
-var index = [0, 1, 2];
+var scene = {
+  terrain: {
+    verts: [],
+    index: [],
+    color: [],
+    buffers: {}
+  }
+}
 
 var a_position = gl.getAttribLocation(program, "a_position");
-gl.enableVertexAttribArray(program, a_position);
 
-var positionBuffer = gl.createBuffer();
-// gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+var a_color = gl.getAttribLocation(program, "a_color");
 
-var indexBuffer = gl.createBuffer();
-// gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-// gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(index), gl.STATIC_DRAW);
+var camera = {
+  position: [0, 0, 0],
+  target: [0, 0, 0],
+  up: [0, 1, 0],
+  perspective: mat4.create()
+};
 
-var cameraCoords = [10, 3, -10];
-var target = [0, 0, 0];
-var up = [0, 1, 0];
-
-var perspective = mat4.create();
-mat4.identity(perspective);
-mat4.perspective(perspective, 45 * Math.PI / 180, canvas.width / canvas.height, .1, 300);
+mat4.identity(camera.perspective);
+mat4.perspective(camera.perspective, 45 * Math.PI / 180, canvas.width / canvas.height, .1, 300);
 var u_perspective = gl.getUniformLocation(program, "u_perspective");
 
 var u_matrix = gl.getUniformLocation(program, "u_matrix");
@@ -79,33 +83,36 @@ var render = function(time) {
   
   var distance = 10;
   
-  cameraCoords = [
+  camera.position = [
     Math.sin(time) * distance,
     5,
     Math.cos(time) * distance
-  ]
+  ];
   
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   
   var matrix = mat4.create();
-  mat4.lookAt(matrix, cameraCoords, target, up);
+  mat4.lookAt(matrix, camera.position, camera.target, camera.up);
   
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.enableVertexAttribArray(a_position);
+  gl.bindBuffer(gl.ARRAY_BUFFER, scene.terrain.buffers.position);
   gl.vertexAttribPointer(a_position, 3, gl.FLOAT, false, 0, 0);
   
-  gl.uniformMatrix4fv(u_perspective, false, perspective);
+  gl.enableVertexAttribArray(a_color);
+  gl.bindBuffer(gl.ARRAY_BUFFER, scene.terrain.buffers.color);
+  gl.vertexAttribPointer(a_color, 3, gl.FLOAT, false, 0, 0);
+  
+  gl.uniformMatrix4fv(u_perspective, false, camera.perspective);
   gl.uniformMatrix4fv(u_matrix, false, matrix);
   
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, scene.terrain.buffers.index);
   
-  gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+  gl.drawElements(gl.TRIANGLES, scene.terrain.index.length, gl.UNSIGNED_SHORT, 0);
   // gl.drawArrays(gl.TRIANGLES, 0, verts.length / 3);
   
   requestAnimationFrame(render);
 };
-
-// requestAnimationFrame(render);
 
 var noise = new Image();
 noise.src = "noise.png";
@@ -116,22 +123,37 @@ noise.onload = function() {
   heightmap.height = noise.height;
   context.drawImage(noise, 0, 0, noise.width, noise.height);
   var imageData = context.getImageData(0, 0, noise.width, noise.height);
-  var getHeight = function(x, y) {
-    x = Math.floor(x * noise.width);
-    y = Math.floor(y * noise.height);
-    return imageData.data[(y * noise.height + x) * 4] / 255;
+  var getPixel = function(x, y) {
+    x = Math.floor(x * (noise.width - 1));
+    y = Math.floor(y * (noise.height - 1));
+    var index = (y * noise.height + x) * 4;
+    return imageData.data.slice(index, index + 4);
   }
   
-  //create the plane
+  // create the plane
+  // points along each axis
   var interval = 100;
+  // size in scene units
   var size = 10;
-  verts = new Array(interval ** 2);
+  scene.terrain.verts = new Array(interval ** 2);
+  scene.terrain.color = new Array(interval ** 2);
+  // polys along each axis
+  var edges = interval - 1;
+  // element index buffer
+  scene.terrain.index = new Array((edges ** 2) * 6);
+  var { verts, index, color } = scene.terrain;
   
-  //generate points
+  //generate points, color attribute
   for (x = 0; x < interval; x++) {
     for (z = 0; z < interval; z++) {
       var i = ((x * interval + z) * 3);
-      var height = getHeight(x / (interval - 1), z / (interval - 1)) * 2;
+      var u = x / (interval - 1);
+      var v = z / (interval - 1);
+      var pixel = getPixel(u, v);
+      var height = pixel[0] / 255 * 2;
+      color[i] = Math.random();
+      color[i+1] = Math.random();
+      color[i+2] = Math.random();
       verts[i] = x / (interval - 1) * size - (size / 2);
       verts[i+1] = height;
       verts[i+2] = z / (interval - 1) * size - (size / 2);
@@ -139,8 +161,6 @@ noise.onload = function() {
   }
   
   //generate index list
-  var edges = interval - 1;
-  index = new Array((edges ** 2) * 6);
   for (var i = 0; i < edges; i++) {
     for (var j = 0; j < edges; j++) {
       var k = (i * edges + j) * 6;
@@ -154,10 +174,16 @@ noise.onload = function() {
     }
   }
   
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  scene.terrain.buffers.position = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, scene.terrain.buffers.position);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
   
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  scene.terrain.buffers.color = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, scene.terrain.buffers.color);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW);
+  
+  scene.terrain.buffers.index = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, scene.terrain.buffers.index);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(index), gl.STATIC_DRAW);
   
   requestAnimationFrame(render);
